2a5111d26c6df511fc79678e6532828a494c35e1
EtomicBomb <ethan@ethan.ws>
2024-10-12 21:33:17 -0400

    started to work on media-conv

--- media-conv/cleanup.sh
+++ media-conv/cleanup.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-cd "$(realpath $(dirname "$0"))"
-rm -rf ./inputs
-rm -rf ./outputs
+REPO_TOP=$(git rev-parse --show-toplevel)
+results_dir="${REPO_TOP}/media-conv/results"
+
+echo "Cleaning up outputs..."
+rm -rf $results_dir
+
