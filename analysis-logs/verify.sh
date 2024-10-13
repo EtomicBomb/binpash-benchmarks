2715623303bd09fe34530eccf6671657b12c73ae
EtomicBomb <ethan@ethan.ws>
2024-10-12 21:25:31 -0400

    work on analysis-logs conforming to the interface

--- analysis-logs/verify.sh
+++ analysis-logs/verify.sh
@@ -2,11 +2,12 @@
 
 REPO_TOP=$(git rev-parse --show-toplevel)
 
-eval_dir="${REPO_TOP}/analysis-logs/"
+eval_dir="${REPO_TOP}/analysis-logs"
 results_dir="${eval_dir}/results"
 input_dir="${eval_dir}/input"
+checksum="${eval_dir}/hashes/results.md5"
 
-if [ "$(md5sum $results_dir/* | awk '{print $1}')" == "$(cat $input_dir/checksum.md5 | awk '{print $1}')" ];
+if [ "$(md5sum $results_dir/* | awk '{print $1}')" == "$(cat $checksum)" ];
 then
     echo "Valid"
 else
